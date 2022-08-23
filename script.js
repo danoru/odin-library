e5d41d1c746091803f5c4195bab43ca02416f7bc
Daniel Mendez <awilddanoru@gmail.com>
2022-08-23 16:09:03 -0700

    Added HTML & CSS skeleton for project

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
+class Game {
+  constructor(title, rating, weight, players, playTime, isPlayed) {
+    this.title = title;
+    this.rating = rating;
+    this.weight = weight;
+    this.players = players;
+    this.playTime = playTime;
+    this.isPlayed = isPlayed;
+  }
+}
+function addGametoLibrary() {
+  const newGame = new Game(title, rating, weight, players, playTime, isPlayed);
+  myLibrary.push(newGame)
 }
 
-function addBooktoLibrary() {
-  alert(myLibrary)
-}
-
+addGametoLibrary("Ark Nova", "8.6", "3.71", "1-4", "90-150 Min", false);
